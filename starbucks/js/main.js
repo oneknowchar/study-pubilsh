3b47ecd600b9fd56cdebfe48d723c1652dbc71bd
oneknowchar <oneknowchar@gmail.com>
2024-10-13 23:05:33 +0900

    뱃지 구현, gsap.to() 라이브러리 사용

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -14,3 +14,26 @@ searchInputEl.addEventListener('blur', function(){
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder','');
 });
+
+const badgeEl = document.querySelector('header .badges');
+
+//gsap.to(요소, 지속시간, 옵션);
+window.addEventListener('scroll', _.throttle(function () {
+  if (window.scrollY > 500) {
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    })
+    gsap.to(toTopEl, .2, {
+      x: 0
+    })
+  } else {
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    })
+    gsap.to(toTopEl, .2, {
+      x: 100
+    })
+  }
+}, 300));
